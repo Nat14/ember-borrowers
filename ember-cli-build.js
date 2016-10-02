a7bf4b023a83bc7c334f30e407c1a44ce9c63e5b
nat14 <naryucha14@gmail.com>
2016-10-02 15:58:40 -0700

    Back to use ic-ajax via addon

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -26,8 +26,6 @@ module.exports = function(defaults) {
 
   app.import('bower_components/moment/moment.js');
 
-  app.import('bower_components/ic-ajax/dist/named-amd/main.js');
-
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
